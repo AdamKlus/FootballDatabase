4d6069f020f9973b467bfc23fce85075f49676ff
Adam Klus <apklus@gmail.com>
2021-10-20 21:59:29 +0100

    Create etl.py

---
+++ etl.py
@@ -0,0 +1,27 @@
+import configparser
+import psycopg2
+from functions import drop_tables, create_tables, load_staging_table, insert_tables,\
+     scrape_website, truncate_tables, update_tables
+
+if __name__ == "__main__":
+
+    # get config
+    config = configparser.ConfigParser()
+    config.read('dwh.cfg')
+
+    # connect to database
+    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
+    cur = conn.cursor()
+
+    # ETL FOR HISTORICAL DATA
+    drop_tables(cur, conn)
+    create_tables(cur, conn)
+    load_staging_table(cur, conn) # insert data from S3 bucket into staging table
+    insert_tables(cur, conn) # populate final tables with data from staging table
+
+    # ETL FOR NEW DATA
+    truncate_tables(cur, conn) # truncating staging table before pupulating
+    scrape_website(cur, conn) # scraping website betexplorer.com for new data and inserting into staging table
+    update_tables(cur, conn) # update final tables with data from staging table
+
+    conn.close()
